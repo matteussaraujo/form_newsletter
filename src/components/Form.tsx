1b7b56f4efad3a822ba4db13ed568319c8545587
matteussaraujo <mateusdev36@gmail.com>
2025-01-02 22:51:50 -0300

    Estilizando componentes e criação de um arquivo de configuração para ajustar as regras ou comportamentos de uma ferramenta de análise de código

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
 const Form = () => {
   return (
-    <div>Form</div>
-  )
+    <form className="flex flex-col gap-3">
+        <div className="flex flex-col">
+        <label className="text-sm" htmlFor="name">Nome</label>
+        <input type="text" placeholder="Digite o seu nome" className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400" />
+        </div>
+
+        <div className="flex flex-col">
+        <label className="text-sm" htmlFor="email">E-mail</label>
+        <input type="email" placeholder="Digite o seu e-mail" className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400" />
+        </div>
+
+        <div className="flex flex-col"> 
+            <a href="#" className="text-xs underline mb-2">Leia os termos</a>
+            <div className="flex gap-2 items-center">
+            <input type="checkbox" />
+            <label className="text-sm" htmlFor="agree">Concordo com os termos</label>
+            </div>
+        </div>
+        <button type="submit" className="bg-slate-600 hover:bg-slate-500 font-medium text-sm py-2 px-4 rounded-lg text-white">Cadastrar</button>
+    </form>  
+    )
 }
 
 export default Form
